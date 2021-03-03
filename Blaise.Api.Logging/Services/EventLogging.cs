193d22affc2211752080d2278d4f56d8c0eb31ce
Nik Webb <nik.webb@ons.gov.uk>
2021-03-03 09:25:26 +0000

    Added logs to go to windows event viewer

---
+++ Blaise.Api.Logging/Services/EventLogging.cs
@@ -0,0 +1,19 @@
+﻿using Blaise.Api.Contracts.Interfaces;
+using System;
+using System.Diagnostics;
+
+namespace Blaise.Api.Logging.Services
+{
+    public class EventLogging : ILoggingService
+    {
+        public void LogError(string message, Exception exception)
+        {
+            EventLog.WriteEntry("Rest API", message, EventLogEntryType.Error, 0,0, Convert.ToBase64CharArray(exception.InnerException));
+        }
+
+        public void LogInfo(string message)
+        {
+            EventLog.WriteEntry("Rest API", message, EventLogEntryType.Information);
+        }
+    }
+}
