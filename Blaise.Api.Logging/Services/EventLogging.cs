09936b7332eaa1aa07a75faec51b7d27890f077b
Nik Webb <nik.webb@ons.gov.uk>
2021-03-04 16:14:44 +0000

    Added rest api to the start of the event for gcp

--- Blaise.Api.Logging/Services/EventLogging.cs
+++ Blaise.Api.Logging/Services/EventLogging.cs
@@ -9,17 +9,17 @@ namespace Blaise.Api.Logging.Services
     {
         public void LogError(string message, Exception exception)
         {
-            EventLog.WriteEntry("Rest API", $"{message}: {exception.Message}, {exception.InnerException}", EventLogEntryType.Error);
+            EventLog.WriteEntry("Rest API", $"RESTAPI: {message}: {exception.Message}, {exception.InnerException}", EventLogEntryType.Error);
         }
 
         public void LogInfo(string message)
         {
-            EventLog.WriteEntry("Rest API", message, EventLogEntryType.Information);
+            EventLog.WriteEntry("Rest API", $"RESTAPI: {message}", EventLogEntryType.Information);
         }
 
         public void LogWarn(string message)
         {
-            EventLog.WriteEntry("Rest API", message, EventLogEntryType.Warning);
+            EventLog.WriteEntry("Rest API", $"RESTAPI: {message}", EventLogEntryType.Warning);
         }
     }
 }
