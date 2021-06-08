9d2a7a885419317ad8102d765821fff98524dfde
jamie kerr <jamiestephenkerr@gmail.com>
2021-06-08 15:12:24 +0100

    Added admin service and connection model mapper and simplified admin controller

--- Blaise.Api/Controllers/AdminController.cs
+++ Blaise.Api/Controllers/AdminController.cs
@@ -1,8 +1,9 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Web.Http;
 using Blaise.Api.Contracts.Interfaces;
-using Blaise.Api.Contracts.Models.Instrument;
-using Blaise.Nuget.Api.Contracts.Interfaces;
+using Blaise.Api.Contracts.Models.Admin;
+using Blaise.Api.Core.Interfaces.Services;
 using Swashbuckle.Swagger.Annotations;
 
 namespace Blaise.Api.Controllers
@@ -10,30 +11,32 @@ namespace Blaise.Api.Controllers
     [RoutePrefix("api/v1/admin")]
     public class AdminController : BaseController
     {
-        private readonly IBlaiseAdminApi _blaiseAdminApi;
+        private readonly IAdminService _adminService;
         private readonly ILoggingService _loggingService;
 
         public AdminController(
-            IBlaiseAdminApi blaiseAdminApi,
+            IAdminService adminService,
             ILoggingService loggingService) : base(loggingService)
         {
+            _adminService = adminService;
             _loggingService = loggingService;
-            _blaiseAdminApi = blaiseAdminApi;
         }
 
         [HttpGet]
         [Route("connections")]
-        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(int))]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(IEnumerable<OpenConnectionDto>))]
         public IHttpActionResult GetActiveConnections()
         {
-            return Ok(_blaiseAdminApi.ActiveConnections());
+            var openConnections = _adminService.GetOpenConnections();
+
+            return Ok(openConnections);
         }
 
         [HttpDelete]
         [Route("connections")]
         public IHttpActionResult ResetConnections()
         {
-            _blaiseAdminApi.ResetConnections();
+            _adminService.ResetConnections();
             _loggingService.LogInfo("reset all cached blaise connections");
 
             return Ok();
