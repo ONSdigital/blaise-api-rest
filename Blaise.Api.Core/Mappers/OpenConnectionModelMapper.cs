42ed235234aba2a4c246f3aaeb1c5f717e90c756
Jamie Kerr <60973239+jamiestephenkerr@users.noreply.github.com>
2021-06-09 13:30:00 +0100

    Lu 8597 (#69)
    * Updated to use fied connections in NUGET
    
    * Updated to use change in nuget package which will establish a fresh connection when an instrument is re-installed
    
    * Added admin service and connection model mapper and simplified admin controller

---
+++ Blaise.Api.Core/Mappers/OpenConnectionModelMapper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Blaise.Api.Contracts.Models.Admin;
+using Blaise.Api.Core.Interfaces.Mappers;
+using Blaise.Nuget.Api.Contracts.Models;
+
+namespace Blaise.Api.Core.Mappers
+{
+    public class OpenConnectionModelMapper : IOpenConnectionModelMapper
+    {
+        public IEnumerable<OpenConnectionDto> MapTOpenConnectionDtos(IEnumerable<OpenConnectionModel> openConnectionModels)
+        {
+            var openConnectionDtoList = new List<OpenConnectionDto>();
+
+            if (openConnectionModels == null)
+            {
+                return openConnectionDtoList;
+            }
+  
+            foreach (var openConnectionModel in openConnectionModels)
+            {
+                openConnectionDtoList.Add(new OpenConnectionDto
+                {
+                    ConnectionType = openConnectionModel.ConnectionType,
+                    Connections = openConnectionModel.Connections
+                });
+            }
+
+            return openConnectionDtoList;
+        }
+    }
+}
---
+++ Blaise.Api.Tests.Unit/Mappers/OpenConnectionModelMapperTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Blaise.Api.Contracts.Models.Admin;
+using Blaise.Api.Core.Interfaces.Mappers;
+using Blaise.Api.Core.Mappers;
+using NUnit.Framework;
+using Blaise.Nuget.Api.Contracts.Models;
+
+namespace Blaise.Api.Tests.Unit.Mappers
+{
+    public class OpenConnectionModelMapperTests
+    {
+        private IOpenConnectionModelMapper _sut;
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            _sut = new OpenConnectionModelMapper();
+        }
+
+        [Test]
+        public void Given_I_Call_MapTOpenConnectionDtos_With_A_Null_Value_Then_An_Empty_List_Of_OpenConnectionDtos_Is_Returned()
+        {
+            //act
+            var result = _sut.MapTOpenConnectionDtos(null); ;
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<OpenConnectionDto>>(result);
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void Given_I_Have_A_Empty_List_Of_OpenConnectionModels_When_I_Call_MapTOpenConnectionDtos_Then_An_Empty_List_Of_OpenConnectionDtos_Is_Returned()
+        {
+            //arrange
+            var openConnectionModels = new List<OpenConnectionModel>();
+
+            //act
+            var result = _sut.MapTOpenConnectionDtos(openConnectionModels);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<OpenConnectionDto>>(result);
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void Given_I_Have_A_Populated_List_Of_OpenConnectionModels_When_I_Call_MapTOpenConnectionDtos_Then_A_Correct_List_Of_OpenConnectionDtos_Is_Returned()
+        {
+            //arrange
+            var openConnectionModels = new List<OpenConnectionModel>
+            {
+                new OpenConnectionModel { ConnectionType = "TestConnection1", Connections = 10},
+                new OpenConnectionModel { ConnectionType = "TestConnection2", Connections = 2}
+            };
+
+            //act
+            var result = _sut.MapTOpenConnectionDtos(openConnectionModels).ToList();
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<OpenConnectionDto>>(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.True(result.Any(c => 
+                c.ConnectionType == openConnectionModels[0].ConnectionType && c.Connections == openConnectionModels[0].Connections));
+            Assert.True(result.Any(c =>
+                c.ConnectionType == openConnectionModels[1].ConnectionType && c.Connections == openConnectionModels[1].Connections));
+        }
+    }
+}
