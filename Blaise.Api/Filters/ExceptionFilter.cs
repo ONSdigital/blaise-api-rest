9ce1258a986cfc1a3dc7ddb8e2128245d3fad22b
jamie kerr <jamiestephenkerr@gmail.com>
2022-01-05 11:36:18 +0000

    Fixed build issue

--- Blaise.Api/Filters/ExceptionFilter.cs
+++ Blaise.Api/Filters/ExceptionFilter.cs
@@ -2,8 +2,8 @@
 using System.Net;
 using System.Net.Http;
 using System.Web.Http.Filters;
+using Blaise.Api.Configuration;
 using Blaise.Api.Contracts.Interfaces;
-using Blaise.Api.Logging.Services;
 using Blaise.Nuget.Api.Contracts.Exceptions;
 
 namespace Blaise.Api.Filters
