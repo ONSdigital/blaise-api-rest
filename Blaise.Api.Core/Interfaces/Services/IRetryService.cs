d439957cb4256a69792af975540fb2ebf4d10974
jamie kerr <jamiestephenkerr@gmail.com>
2022-04-27 12:55:06 +0100

    Added retry service and implemented for getInstruments call

---
+++ Blaise.Api.Core/Interfaces/Services/IRetryService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Blaise.Api.Core.Interfaces.Services
+{
+    public interface IRetryService<T>
+    {
+        T2 Retry<T1, T2>(Func<T1, T2> method, T1 argument);
+    }
+}
---
+++ Blaise.Api.Core/Services/RetryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Blaise.Api.Core.Interfaces.Services;
+using Polly;
+using Polly.Retry;
+
+namespace Blaise.Api.Core.Services
+{
+    public class RetryService<T> : IRetryService<T> where T: Exception
+    {
+        private const int MaxRetryAttempts = 3;
+        private readonly TimeSpan _pauseBetweenFailures = TimeSpan.FromSeconds(1);
+        private readonly RetryPolicy _retryPolicy;
+
+        public RetryService()
+        {
+            _retryPolicy = Policy
+                .Handle<T>()
+                .WaitAndRetry(MaxRetryAttempts, i => _pauseBetweenFailures);
+        }
+
+        public T2 Retry<T1, T2>(Func<T1, T2> method, T1 argument)
+        {
+            return _retryPolicy.Execute(() => method(argument));
+        }
+    }
+}
