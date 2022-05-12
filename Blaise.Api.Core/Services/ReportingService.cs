46c9fa6459c992424a353895c58659899356a0ba
social-surveys-blaise-concourse <79969802+social-surveys-blaise-concourse@users.noreply.github.com>
2022-05-12 13:14:13 +0100

    Fix behaviour (#159) (#160)
    * Update stub config with retry
    
    * Throw a 404 if field is not found
    
    Co-authored-by: Jamie Kerr <60973239+jamiestephenkerr@users.noreply.github.com>

--- Blaise.Api.Core/Services/ReportingService.cs
+++ Blaise.Api.Core/Services/ReportingService.cs
@@ -62,10 +62,14 @@ namespace Blaise.Api.Core.Services
         private Dictionary<string, string> GetReportFieldData(IEnumerable<string> fieldIds, IDataRecord caseRecord)
         {
             var reportingData = new Dictionary<string, string>();
+            var caseFields = _blaiseCaseApi.GetRecordDataFields(caseRecord);
 
             foreach (var fieldId in fieldIds)
             {
-                reportingData.Add(fieldId, _blaiseCaseApi.GetFieldValue(caseRecord, fieldId).ValueAsText);
+                if (caseFields.Any(f => f.Key.Equals(fieldId, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    reportingData.Add(fieldId, caseFields.FirstOrDefault(f => f.Key.Equals(fieldId, StringComparison.InvariantCultureIgnoreCase)).Value);
+                }
             }
 
             return reportingData;
