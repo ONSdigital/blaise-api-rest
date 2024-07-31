69b673d9bac267ff745e75497ea881b73e8ce391
Jake West <jacob.west@ons.gov.uk>
2024-07-31 10:51:50 +0100

    Added mapper and tests, happy path test failing

--- Blaise.Api.Core/Mappers/EditingDtoMapper.cs
+++ Blaise.Api.Core/Mappers/EditingDtoMapper.cs
@@ -1,14 +1,35 @@
 ﻿using Blaise.Api.Contracts.Models.Edit;
+using Blaise.Api.Core.Extensions;
 using Blaise.Api.Core.Interfaces.Mappers;
+using Blaise.Nuget.Api.Contracts.Interfaces;
 using StatNeth.Blaise.API.DataRecord;
+using System;
+using System.Drawing.Text;
 
 namespace Blaise.Api.Core.Mappers
 {
     public class EditingDtoMapper : IEditingDtoMapper
     {
+        private readonly IBlaiseCaseApi _blaiseCaseApi;
+
+        public EditingDtoMapper(IBlaiseCaseApi blaiseCaseApi)
+        {
+            _blaiseCaseApi = blaiseCaseApi;
+        }
         public EditingDetailsDto MapToEditingDetailsDto(IDataRecord caseRecord)
         {
-            throw new System.NotImplementedException();
+            caseRecord.ThrowExceptionIfNull("caseRecord");                
+
+            var editingDetailsDto = new EditingDetailsDto
+            {
+                PrimaryKey = _blaiseCaseApi.GetFieldValue(caseRecord, "QID.Serial_Number").StringValue,
+                Outcome = (int)_blaiseCaseApi.GetFieldValue(caseRecord, "Admin.HOut").IntegerValue,
+                AssignedTo = _blaiseCaseApi.GetFieldValue(caseRecord, "QEdit.AssignedTo").StringValue,
+                EditedStatus = _blaiseCaseApi.GetFieldValue(caseRecord, "QEdit.edited").StringValue,
+                // TODO
+                Interviewer = "",
+            };
+            return editingDetailsDto;
         }
     }
 }
