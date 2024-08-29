2019f1caaead0ed6067ee5ea0ce85ea0ccc07b0e
jamie kerr <jamiestephenkerr@gmail.com>
2024-08-29 17:38:48 +0100

    Fixed issue where  I removed an import that was needed for release

--- Blaise.Api/Filters/ExceptionFilter.cs
+++ Blaise.Api/Filters/ExceptionFilter.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Web.Http.Filters;
+using Blaise.Api.Configuration;
 using Blaise.Api.Contracts.Interfaces;
 using Blaise.Api.Logging.Services;
 using Blaise.Nuget.Api.Contracts.Exceptions;
